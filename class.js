7cf164b4ce1c5d5639148dcf66c33b82249b7dcd
Wang Zhu <wang@howtelevision.jp>
2015-01-30 12:08:51 +0900

    corrected

--- class.js
+++ class.js
@@ -9,11 +9,11 @@ function Block ( i, j, data ) {
 		west : (j>0) ? data[i][j-1] : false,
 		northWest : (j>0 && i>0) ? data[i-1][j-1] : false,
 		north : (i>0) ? data[i-1][j] : false,
-		northEast : (i>0 && j<data.length-1) ? data[i-1][j+1] : false,
-		east : (j<data.length-1) ? data[i][j+1] : false,
-		southEast : (j<data.length-1 && i<data[0].length-1) ? data[i+1][j+1] : false,
-		south : (i<data[0].length-1) ? data[i+1][j] : false,
-		southWest : (j>0 && i<data[0].length-1) ? data[i+1][j-1] : false,
+		northEast : (i>0 && j<data[0].length-1) ? data[i-1][j+1] : false,
+		east : (j<data[0].length-1) ? data[i][j+1] : false,
+		southEast : (j<data[0].length-1 && i<data.length-1) ? data[i+1][j+1] : false,
+		south : (i<data.length-1) ? data[i+1][j] : false,
+		southWest : (j>0 && i<data.length-1) ? data[i+1][j-1] : false,
 	}
 	for( var item in neighbors ){
 		if( neighbors[item] ) number++;
@@ -21,33 +21,34 @@ function Block ( i, j, data ) {
 	self.$element = $('<div class="block" data-i=' + i + ' data-j=' + j + ' data-status=closed>');
 	self.$element.on('click',function(){
 		if( $(this).attr('data-status')=='closed' ){
+			// console.log(neighbors);
 			$(this).attr('data-status','open');
-			open($(this));
+			self.open();
 		}else{
 			// console.log(i,j)
 		}
-		function open($element){
-			// console.log(isMine);
-			if(isMine){
-				// console.log($element);
-				$element.parent().children('div[data-status=closed]').click();
-				$element.css('background','red');
-			}else{
-				$element.css('background','white');
-				$element.text(number);
-				if(number==0){
-					$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j) + '][data-status=closed]').css('background','yellow').click();
-					$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j) + '][data-status=closed]').css('background','yellow').click();
-				}
+	})
+	self.open = function open(){
+		// console.log(isMine);
+		if(isMine){
+			// console.log($element);
+			self.$element.parent().children('div[data-status=closed]').click();
+			self.$element.css('background','red');
+		}else{
+			self.$element.css('background','white');
+			self.$element.text(number);
+			if(number==0){
+				self.$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i) + '][data-j=' + (j-1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i) + '][data-j=' + (j+1) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i-1) + '][data-j=' + (j) + '][data-status=closed]').css('background','yellow').click();
+				self.$element.parent().children('div[data-i=' + (i+1) + '][data-j=' + (j) + '][data-status=closed]').css('background','yellow').click();
 			}
 		}
-	})
+	}
 	self.$element.on('contextmenu',function(){
 		event.preventDefault();
 		if($(this).attr('data-status')=='mine'){
