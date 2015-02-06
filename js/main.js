a588d10b6f68263a189770b707cc292f1d3bf670
Wang Zhu <wang@howtelevision.jp>
2015-02-06 17:22:39 +0900

    release

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $(function () {
 		var width = $(this).find('#width').val();
 		var height = $(this).find('#height').val();
 		var mine = $(this).find('#mines').val();
+		console.log(height)
 		if(width >= 5 && width <= 50 && height >= 5 && height <= 50 && mine > 0){
 			mine = mine > width * height ? (width * height / 2 ^ 0) : mine;
+			// console.log(mine)
 			board.reset({
 				width : width,
 				height : height,
